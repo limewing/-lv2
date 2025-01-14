40ebe682fbde9100d68529068e689c3c845abdd7
limewing <58874904+limewing@users.noreply.github.com>
2025-01-14 14:34:24 +0900

    Add files via upload

---
+++ running_lv2-4.cs
@@ -0,0 +1,14 @@
+﻿int[] num = { 10, 20, 30, 40, 50 };
+int max = num[0];
+int min = num[0];
+
+for (int i = 1; i < num.Length; i++)
+{
+    if (num[i] > max)
+        max = num[i];
+    if (num[i] < min)
+        min = num[i];
+}
+
+Console.WriteLine($"Max: {max}");
+Console.WriteLine($"Min: {min}");
