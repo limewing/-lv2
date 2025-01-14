50ffaff51d332daff335761e63ab55212f43bada
limewing <58874904+limewing@users.noreply.github.com>
2025-01-14 14:19:05 +0900

    Add files via upload

---
+++ running_lv2-2.cs
@@ -0,0 +1,21 @@
+﻿Random random = new Random();
+int targetNumber = random.Next(1, 101);
+int guess = 0;
+
+do
+{
+    Console.Write("Enter your guess (1-100): ");
+    guess = int.Parse(Console.ReadLine());
+    if (guess < targetNumber)
+    {
+        Console.WriteLine("Too low! Try again.");
+    }
+    else if (guess > targetNumber)
+    {
+        Console.WriteLine("Too high! Try again.");
+    }
+    else
+    {
+        Console.WriteLine("Congratulations! You guessed the number.");
+    }
+} while (guess != targetNumber);
